1562ad3133f1668106c957a0da1b0dcb57dcf2d0
Emile <iyaemile4@gmail.com>
2022-06-06 10:05:00 +0200

    this is python import module

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-
-def main():
-    list_arguments = sys.argv
+if __name__ == '__main__':
+    av = sys.argv
+    l_av = len(av)
     sum = 0
-    for count in range(1, len(list_arguments)):
-        sum += int(list_arguments[count])
-    print("{}".format(sum))
 
+    if l_av > 1:
+        for i in range(1, l_av):
+            sum += int(av[i])
 
-if __name__ == "__main__":
-    main()
+    print(sum)
