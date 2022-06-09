41c999f161d320b37cd82d7a0f17abaa3625effe
Emile <iyaemile4@gmail.com>
2022-06-09 14:23:44 +0200

    this is python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == '__main__':
-    """Prints the argument list passed to the program
 
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
+def star():
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argc))
+        for count in range(1, argc + 1):
+            print("{}: {}".format(count, sys.argv[count]))
 
-    """
-    av = sys.argv
-    l_av = len(av) - 1
 
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
+if __name__ == "__main__":
+    star()
