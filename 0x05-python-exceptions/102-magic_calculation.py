9cffa470953de5cffd496929a54e9d343f9abb9a
Emile <iyaemile4@gmail.com>
2022-06-20 21:30:49 +0200

    this is Function that does exactly the same as a Python bytecode given

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+:#!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -11,3 +11,4 @@ def magic_calculation(a, b):
             result = b + a
             break
     return result
+
