92abe809e4c84c5fa6c4854b4a9711d75d6dbdc2
IYADUKUNZE Emile <80049630+emilenfc@users.noreply.github.com>
2022-09-13 20:08:49 +0200

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """
-changes the name of the State object where id=2 to New Mexico from a database
-"""
-
-import sqlalchemy
+deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa"""
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
+from sqlalchemy.orm import Session
 from model_state import Base, State
 
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))
-    for state in states:
-        session.delete(state)
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    re = session.query(State).filter(State.name.like('%a%'))
+    for row in re:
+        session.delete(row)
     session.commit()
     session.close()
